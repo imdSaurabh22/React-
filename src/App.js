c02834f2a11465ebced2c69c9430f964ce2d7557
Saurabh Chauhan <imdsaurabh30@gmail.com>
2024-07-29 09:15:49 -0700

    use-state-method

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
-import React from "react";
+import React ,{useState} from "react";
 
 function App() {
-  return <div />;
+
+  const[count , stateCount]= useState(0)
+
+    function increase(){
+      stateCount(count+1);
+    }
+    function decrease(){
+      stateCount(count-1);
+    }    
+  return  (
+  <div className="container">
+  <h1>{count}</h1>
+  <button onClick={increase}>+</button>
+  <button onClick={decrease}>-</button>
+  </div>);
 }
 
 export default App;
+
+
+
+
+
+
+// import React from "react";
+
+// function App() {
+//   return <div />;
+// }
+
+// export default App;
 
